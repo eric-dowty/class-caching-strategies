fec970b3cce8c75a69d5d7222c348a092133ffac
end3rs-army <eric.m.dowty@gmail.com>
2015-06-09 10:57:28 -0600

    caching based on callback not a good idea for all use cases. Instead assign a cache key in the cache block

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,10 @@ class Item < ActiveRecord::Base
   has_many :order_items
   has_many :orders, through: :order_items
 
-  after_save    :clear_cache
-  after_create  :clear_cache
-  after_destroy :clear_cache
+  # after_save    :clear_cache
+  # after_create  :clear_cache
+  # after_destroy :clear_cache
 
-  include Clear
+  # include Clear
 
 end
